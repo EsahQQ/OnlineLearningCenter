1c40cd5ddf2b8aad516072c66299dcff83da3977
Kovalev Ivan <kovalevvanc@gmail.com>
2025-10-20 22:20:25 +0300

    Add: controllers for courses and instructors

---
+++ OnlineLearningCenter.Web/Controllers/CoursesController.cs
@@ -0,0 +1,154 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OnlineLearningCenter.BusinessLogic.DTOs;
+using OnlineLearningCenter.BusinessLogic.Services;
+using System.Threading.Tasks;
+
+namespace OnlineLearningCenter.Web.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // Внедряем сервисы и AutoMapper вместо DbContext
+        private readonly ICourseService _courseService;
+        private readonly IInstructorService _instructorService;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseService courseService, IInstructorService instructorService, IMapper mapper)
+        {
+            _courseService = courseService;
+            _instructorService = instructorService;
+            _mapper = mapper;
+        }
+
+        // GET: Courses - добавляем параметры для фильтрации
+        public async Task<IActionResult> Index(string? category, string? difficulty, int? instructorId)
+        {
+            var courses = await _courseService.GetActiveCoursesAsync(category, difficulty, instructorId);
+
+            // ViewBag.Instructors = ...
+
+            return View(courses); 
+        }
+
+        // GET: Courses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _courseService.GetCourseByIdAsync(id.Value);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course); 
+        }
+
+        // GET: Courses/Create
+        public async Task<IActionResult> Create()
+        {
+            // вспомогательный метод для загрузки преподавателей
+            await PopulateInstructorsDropDownList();
+            return View();
+        }
+
+        // POST: Courses/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // Вместо Entity Course принимаем CreateCourseDto
+        public async Task<IActionResult> Create(CreateCourseDto courseDto)
+        {
+            // 3. ModelState.IsValid проверяет атрибуты валидации из CreateCourseDto
+            if (ModelState.IsValid)
+            {
+                await _courseService.CreateCourseAsync(courseDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Если модель невалидна, снова загружаем преподавателей и возвращаем View с теми же данными
+            await PopulateInstructorsDropDownList(courseDto.InstructorId);
+            return View(courseDto);
+        }
+
+        // GET: Courses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _courseService.GetCourseByIdAsync(id.Value);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            // нужно получить InstructorId для выпадающего списка.
+
+            // Для формы редактирования нужен объект с полями, которые пользователь будет менять.
+            var courseToEdit = _mapper.Map<CreateCourseDto>(course);
+
+            await PopulateInstructorsDropDownList(course.InstructorId); // ID текущего преподавателя
+            return View(courseToEdit);
+        }
+
+        // POST: Courses/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CreateCourseDto courseDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _courseService.UpdateCourseAsync(id, courseDto);
+                }
+                catch (KeyNotFoundException)
+                {
+                    return NotFound();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            await PopulateInstructorsDropDownList(courseDto.InstructorId);
+            return View(courseDto);
+        }
+
+        // GET: Courses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _courseService.GetCourseByIdAsync(id.Value);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _courseService.DeleteCourseAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Вспомогательный метод
+        private async Task PopulateInstructorsDropDownList(object? selectedInstructor = null)
+        {
+            var instructors = await _instructorService.GetAllInstructorsAsync();
+            ViewBag.InstructorId = new SelectList(instructors, "InstructorId", "FullName", selectedInstructor);
+        }
+    }
+}
---
+++ OnlineLearningCenter.Web/Controllers/InstructorsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineLearningCenter.BusinessLogic.DTOs;
+using OnlineLearningCenter.BusinessLogic.Services;
+using System.Threading.Tasks;
+
+namespace OnlineLearningCenter.Web.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        private readonly IInstructorService _instructorService;
+
+        public InstructorsController(IInstructorService instructorService)
+        {
+            _instructorService = instructorService;
+        }
+
+        // GET: Instructors
+        public async Task<IActionResult> Index()
+        {
+            var instructors = await _instructorService.GetAllInstructorsAsync();
+
+            return View(instructors);
+        }
+
+        // GET: Instructors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            // var instructor = await _instructorService.GetInstructorByIdAsync(id.Value);
+            // if (instructor == null) return NotFound();
+            // return View(instructor);
+            return Content($"Details for instructor {id} will be here.");
+        }
+
+        // GET: Instructors/Create
+        public IActionResult Create()
+        {
+            return Content("Create form for instructor will be here.");
+        }
+
+        // POST: Instructors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(InstructorDto instructor) 
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Instructors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            return Content($"Edit form for instructor {id} will be here.");
+        }
+
+        // POST: Instructors/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, InstructorDto instructor) 
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Instructors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            return Content($"Delete confirmation for instructor {id} will be here.");
+        }
+
+        // POST: Instructors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
