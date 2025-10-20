2c840b4e31a4e5c78dd0830d3145f9782d4c89da
Kovalev Ivan <kovalevvanc@gmail.com>
2025-10-20 15:52:12 +0300

    Add: mappingProfile.cs

---
+++ OnlineLearningCenter.BusinessLogic/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using OnlineLearningCenter.BusinessLogic.DTOs;
+using OnlineLearningCenter.DataAccess.Entities;
+
+namespace OnlineLearningCenter.BusinessLogic.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Instructor, InstructorDto>();
+
+            CreateMap<CreateCourseDto, Course>();
+
+            CreateMap<Course, CourseDto>()
+                .ForMember(
+                    dest => dest.InstructorName,
+                    opt => opt.MapFrom(src => src.Instructor.FullName)
+                );
+
+            // Здесь будут другие правила
+        }
+    }
+}
