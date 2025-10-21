6e4fe68b1e6a58180069c8eede93e333e330766d
Kovalev Ivan <kovalevvanc@gmail.com>
2025-10-21 15:05:13 +0300

    Add: StudentCourseProgressDto.cs

---
+++ OnlineLearningCenter.BusinessLogic/DTOs/StudentCourseProgressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineLearningCenter.BusinessLogic.DTOs;
+
+public class StudentCourseProgressDto
+{
+    public int CourseId { get; set; }
+    public string CourseTitle { get; set; } = string.Empty;
+    public decimal ModulesCompletedProgress { get; set; }
+    public double AverageTestScore { get; set; }
+}
