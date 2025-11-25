e8b63529ad17f289aef0b7842bab2d3cfc38d594
Kovalev Ivan <kovalevvanc@gmail.com>
2025-11-25 20:55:15 +0300

    Add: controller, viewmodels, views for users

---
+++ OnlineLearningCenter.Web/Controllers/UsersController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineLearningCenter.Web.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineLearningCenter.Web.Controllers;
+
+
+[Authorize(Roles = "Admin")]
+public class UsersController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UsersController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    // GET: /Users
+    public async Task<IActionResult> Index()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        return View(users);
+    }
+
+    // GET: Users/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Users/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(CreateUserViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email, EmailConfirmed = true };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "User");
+                return RedirectToAction(nameof(Index));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+        return View(model);
+    }
+
+    // GET: Users/ManageRoles/GUID
+    public async Task<IActionResult> ManageRoles(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null) return NotFound();
+
+        var viewModel = new ManageUserRolesViewModel
+        {
+            UserId = user.Id,
+            UserName = user.UserName,
+            Roles = new List<RoleViewModel>()
+        };
+
+        var allRoles = await _roleManager.Roles.ToListAsync();
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        foreach (var role in allRoles)
+        {
+            viewModel.Roles.Add(new RoleViewModel
+            {
+                RoleName = role.Name,
+                IsSelected = userRoles.Contains(role.Name)
+            });
+        }
+
+        return View(viewModel);
+    }
+
+    // POST: Users/ManageRoles/GUID
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> ManageRoles(ManageUserRolesViewModel model)
+    {
+        var user = await _userManager.FindByIdAsync(model.UserId);
+        if (user == null) return NotFound();
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        await _userManager.RemoveFromRolesAsync(user, userRoles);
+        await _userManager.AddToRolesAsync(user, model.Roles.Where(r => r.IsSelected).Select(r => r.RoleName));
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Users/Delete/GUID
+    public async Task<IActionResult> Delete(string id)
+    {
+        if (id == null) return NotFound();
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null) return NotFound();
+        return View(user);
+    }
+
+    // POST: Users/Delete/GUID
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user != null)
+        {
+            if (_userManager.GetUserId(User) == id)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            await _userManager.DeleteAsync(user);
+        }
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ OnlineLearningCenter.Web/ViewModels/UserManagementViewModels.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineLearningCenter.Web.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string RoleName { get; set; } = string.Empty;
+        public bool IsSelected { get; set; }
+    }
+    public class ManageUserRolesViewModel
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public List<RoleViewModel> Roles { get; set; } = new();
+    }
+
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Пароль обязателен")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
